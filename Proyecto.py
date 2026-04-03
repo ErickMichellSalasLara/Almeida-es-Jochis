0aa5d2a9a31d5fd400204303e08aaaa594ac4f98
ErickMichellSalasLara <st4382@utr.edu.mx>
2026-04-03 17:10:55 -0600

    Add: Se añadio el menu principal del juego y sus respectivos assets

--- Proyecto.py
+++ Proyecto.py
@@ -1,22 +1,116 @@
 # Archivo donde se hara el proyecto
-import pygame, random, numpy as np
+import pygame, random, sys
 
-WHITE = (255, 255, 255)
+SCREEN_WIDTH = 1280 #Ancho
+SCREEN_HEIGHT = 700 #Largo
+WHITE = (255, 255, 255) #Blanco
 
-pygame.init()
+pygame.display.set_caption("POKEUTR") #Titulo de la ventana del juego
 
-size = (800, 600)
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode(size)
+def get_font(size): #Funcion para obtener la fuente del texto, la cual se usara en el menu y en el juego
+    return pygame.font.Font("Fonts/TEXTO_MENU.ttf", size) # Se carga la fuente del texto, y se devuelve la fuente con el tamaño especificado
 
-game_over = False
+def get_sounds(): #Funcion para cargar los sonidos del juego, la cual se usara en el juego
+    pass
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+#Esta clase nos sirve para los botones del menu, como el boton de jugar, el boton de opciones etc
+#La clase la copie y pegue de un video tutorial que encontre en youtube XD
+class Button():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-    screen.fill(WHITE)
-    pygame.display.flip()
-    clock.tick(60)
-pygame.quit()
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
+
+class Pokemones(pygame.sprite.Sprite): #Se crea la clase de los pokemones, usando una subclase de Sprite
+    def __init__(self): #Iniciamos la clase y la super clase
+        super().__init__() # La super clase es la clase Sprite, la cual nos da acceso a sus metodos y atributos
+        
+
+class Game(object):
+    def __init__(self): # Iniciamos la clase
+        self.game_over = False # Variable para saber si el juego se ha terminado o no
+        self.score = 0 # Variable para llevar la puntuacion del jugador
+        
+    def display_frame(self, screen):
+        screen.fill(WHITE)
+        
+    #Esta funcion será toda mi logica del juego.
+    def play(self):
+        # Aqui se hara el codigo del juego, como la logica del juego, el movimiento de los personajes, las colisiones etc
+        while not self.game_over:
+        
+        
+            pass
+    def MainMenu(self, screen):
+        screen.blit(pygame.image.load("Sprites/background_menu.jpg").convert()) # Se carga la imagen de fondo del menu, y se dibuja en la pantalla
+        menu_mouse_pos = pygame.mouse.get_pos()
+    
+        menu_text = get_font(100).render("POKE - UTR", True, "#8d6922")
+        menu_rect = menu_text.get_rect(center=(SCREEN_WIDTH/2, 200)) # Menu rect es el rectangulo del menu, el cual se posiciona en el centro de la pantalla, a 100 pixeles de altura
+    
+        play_button = Button(image=pygame.image.load("Assets/Play Rect.png"), pos=(SCREEN_WIDTH/2, 400), text_input="PLAY", font=get_font(75), base_color="#d8b265", hovering_color="White")
+        quit_button = Button(image=pygame.image.load("Assets/Quit Rect.png"), pos=(SCREEN_WIDTH/2, 550), text_input="QUIT", font=get_font(75), base_color="#d8b265", hovering_color="White")
+    
+        screen.blit(menu_text, menu_rect) # Se dibuja el texto del menu en la pantalla, usando el rectangulo del menu para posicionarlo
+        for button in [play_button, quit_button]: # Se dibujan los botones del menu en la pantalla, usando el metodo update de la clase Button
+            button.changeColor(menu_mouse_pos) # Si el mouse esta sobre el boton, se cambia el color del texto del boton, usando el metodo changeColor de la clase Button
+            button.update(screen) # Se dibuja el boton en la pantalla, usando el metodo update de la clase Button
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN: # Si se hace click con el mouse
+                if play_button.checkForInput(menu_mouse_pos): # Si el mouse esta sobre el boton de play, se llama a la funcion main, la cual es la funcion principal del juego
+                    main()
+                if quit_button.checkForInput(menu_mouse_pos): # Si el mouse esta sobre el boton de quit, se cierra el juego
+                    pygame.quit()
+                    sys.exit()
+        
+        pygame.display.update() # Se actualiza la pantalla para que se dibujen los cambios realizados
+        pass
+
+def main():
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("POKEUTR") # Se establece el titulo de la ventana
+    
+    done = False
+    clock = pygame.time.Clock() # Se crea un reloj para controlar los frames por segundo del juego
+
+    game = Game() # Se crea una instancia de la clase Game, para poder acceder a sus metodos y atributos
+    
+    while not done:
+        game.MainMenu(screen) # Se llama a la funcion MainMenu de la clase Game, para mostrar el menu del juego
+        clock.tick(60) # Se establece el limite de frames por segundo del juego a 60
+    pygame.quit() # Se cierra el juego
+    
+    
+# El if servira solamente para ejecutar la funcion main, y no ejecutar el codigo si se importa este archivo como un modulo
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Almeida-es-Jochis
 
-Este proyecto essta basado en Pokemon, pero solo en el estilo de pelea ya que hacer el rpg sería más complicado
+ - Ocupamos un sistema de Puntuacion por cada ronda que se gane n el juego
+ - Un menu donde se pueda dar a "Jugar"
+ - Los "pokemones" los cuales se haran e una herencia
+ - Encapsulamiento de los valores en cada funcion
+ - Sistema de combates por turnos
+ - Selección de ataques de los pokemones (Cada uno con 4 ataques)
+ - Sistema de conters entre tipos pokemon ( Agua >>> Fuego ) pero con tipos personalizados de la uni
+ - Un maximo de 10 pokemones y 5 entrenadores estaria bien
+ - Sistema de game over
+ - Seleccion individual o random de pokemones
+ - Que el juego pida el nombre de la persona que jugara para asi poder referirse a el/ella
+ - Sonidos (En general)
